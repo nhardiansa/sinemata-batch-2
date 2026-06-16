a1fd9f4341ed6bd92990e3694556459eb479aaec
Nabil <nhardiansa@gmail.com>
2026-06-16 21:57:05 +0800

    feat: add @sglara/cn dependency and refactor Footer component to use cn for class management

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,17 @@
+import { cn } from "@sglara/cn";
+
 function Footer() {
   return (
-    <div className="footer border-t border-[#2A2F37] pt-4.5 pb-12 mt-20">
+    <div
+      className={cn(
+        // name component via class
+        "footer",
+        // styling display
+        "border-t border-[#2A2F37]",
+        // layout setting
+        "pt-4.5 pb-12 mt-20",
+      )}
+    >
       <div className="wrapper-footer max-w-7xl mx-auto flex justify-between items-center text-[#6B7280]">
         <span>Sinemata · Final Project Bootcamp React JS</span>
         <span>Powered by TMDB API · Data fiktif untuk preview</span>
